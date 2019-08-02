65d2791a77e5a53853fbeb7ae2d0b30bd87a43af
Ubuntu <ubuntu@ip-172-31-95-197.ec2.internal>
2019-08-02 04:25:24 +0000

    working for ubuntu

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-mac_check = `uname`
+mac_check=`uname`
 if [[ $mac_check == "Darwin" ]]; then
 	echo "Please install zsh using Homebrew or a similar package manager"
 	exit 127 # command not found, since the next command would fail
 fi
 
-zsh_check = `which zsh`
-if [[ $zsh_check -eq 0 ]]; then
+which zsh
+if [[ $? -eq 0 ]]; then
 	echo "zsh already installed"
 else
-	distro = `cat /etc/os-release | grep ID_LIKE`
+	distro=`cat /etc/os-release | grep ID_LIKE`
 	if [[ $distro =~ "debian" ]]; then
 		echo "\nI will now ask for your sudo privileges to install zsh"
 		# apt-get for backwards compatibility
@@ -21,16 +21,19 @@ else
 	elif [[ $distro =~ "suse" ]]; then
 		echo "\nI will now ask for your sudo privileges to install zsh"
 		sudo zypper refresh && sudo zypper install -y zsh
+	fi
 fi
 
-git_check = `which git`
-if [[ $git_check -eq 0 ]]; then
-	sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/install.sh -O -)"
-	git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-	git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}:~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-	sed -i '/plugins=/s/.*/plugins=(git zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting)' ~/.zshrc
+which git
+if [[ $? -eq 0 ]]; then
+	wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+	chmod +x install.sh
+	`./install.sh --unattended`
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	rm install.sh
+	sed -i '/plugins=(git)/c\plugins=(git zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting)' ~/.zshrc
 else
 	echo "\nPlease install git and try again"
 fi
