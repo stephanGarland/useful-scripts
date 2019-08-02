8eb59bbcb86ec275b44e68711633f159964a9f28
Stephan Garland <sgarland@utexas.edu>
2019-08-01 22:13:29 -0500

    modified git install

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -22,20 +22,15 @@ else
 		echo "\nI will now ask for your sudo privileges to install zsh"
 		sudo zypper refresh && sudo zypper install -y zsh
 fi
+
 git_check = `which git`
-if [[ $ git_check -eq 0 ]]; then
+if [[ $git_check -eq 0 ]]; then
 	sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/install.sh -O -)"
 	git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 	git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM}:~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-	sed '/plugins=/s/.*/plugins=(git zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting)' ~/.zshrc
+	sed -i '/plugins=/s/.*/plugins=(git zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting)' ~/.zshrc
 else
-	echo "\nGit not found, installing"
-	if [[ $distro =~ "debian" ]]; then
-		sudo apt-get install -y git
-	elif [[ $distro =~ "fedora" ]]; then
-		sudo yum install -y git
-	elif [[ $distro =~ "suse" ]]; then
-		sudo zypper install -y git
+	echo "\nPlease install git and try again"
 fi
