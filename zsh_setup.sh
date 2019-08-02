04deb8e8066c12157704e1ae3a4c92e5a6972477
Stephan Garland <stephan.marc.garland@gmail.com>
2019-08-02 05:13:40 +0000

    added chsh

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -2,24 +2,24 @@
 
 mac_check=`uname`
 if [[ $mac_check == "Darwin" ]]; then
-	echo "Please install zsh using Homebrew or a similar package manager"
+	printf "\nPlease install zsh using Homebrew or a similar package manager"
 	exit 127 # command not found, since the next command would fail
 fi
 
 which zsh
 if [[ $? -eq 0 ]]; then
-	echo "zsh already installed"
+	printf "\nzsh already installed"
 else
 	distro=`cat /etc/os-release | grep ID_LIKE`
 	if [[ $distro =~ "debian" ]]; then
-		echo "\nI will now ask for your sudo privileges to install zsh"
+		printf "\nI will now ask for your sudo privileges to install zsh"
 		# apt-get for backwards compatibility
 		sudo apt-get update && sudo apt-get install -y zsh
 	elif [[ $distro =~ "fedora" ]]; then
-		echo "\nI will now ask for your sudo privileges to install zsh"
+		printf "\nI will now ask for your sudo privileges to install zsh"
 		sudo yum update && sudo yum install -y zsh
 	elif [[ $distro =~ "suse" ]]; then
-		echo "\nI will now ask for your sudo privileges to install zsh"
+		printf "\nI will now ask for your sudo privileges to install zsh"
 		sudo zypper refresh && sudo zypper install -y zsh
 	fi
 fi
@@ -35,5 +35,8 @@ if [[ $? -eq 0 ]]; then
 	rm install.sh
 	sed -i '/plugins=(git)/c\plugins=(git zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting)' ~/.zshrc
 else
-	echo "\nPlease install git and try again"
+	printf "\nPlease install git and try again"
 fi
+
+sudo chsh -s `which zsh` `whoami` &> /dev/null
+zsh
