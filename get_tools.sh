3dd88b2708b57d4952544c6dcd86c748fdc018af
Stephan Garland <stephan.garland@logicmonitor.com>
2020-04-08 10:56:35 -0500

    changed case to if

--- get_tools.sh
+++ get_tools.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-read -p "Do you want to update all packages (Y/N)?" updateCheck
-case $updateCheck in
-    [Yy]*) sudo "$pakman" "$upd" -y
-    [Nn]*) printf"\nOK, skipping update"
-esac
+read -p "Do you want to update all packages (Y/N)? " -n 1 -r
+
+if [[ $REPLY =~ ^[Yy]$ ]] then
+    sudo "$pakman" "$upd" -y
+else
+    printf"\nOK, skipping update"
+fi
 printf "\nInstalling/checking git, htop, micro, mc, and tree\n"
 sudo "$pakman" install -y git htop mc tree
 
